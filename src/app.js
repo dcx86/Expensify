2456aee0617ef8de97ff1e104f9d8f4b38e72b68
Dragosh Cr <dragosh.craciun@gmail.com>
2019-07-31 13:16:47 +0200

    Dropdown fro Picking SortBy

--- src/app.js
+++ src/app.js
@@ -4,20 +4,15 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gass bill' }))
-store.dispatch(setTextFilter('gass'))
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'))
-}, 3000)
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gass bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
